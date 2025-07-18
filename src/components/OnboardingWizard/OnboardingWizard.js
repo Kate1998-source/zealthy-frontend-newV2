3395462c9ec0458252f788fe9e4daa10fda5c25e
Kate1998-source <75168937+Kate1998-source@users.noreply.github.com>
2025-07-18 09:15:26 -0500

    Update OnboardingWizard.js

--- src/components/OnboardingWizard/OnboardingWizard.js
+++ src/components/OnboardingWizard/OnboardingWizard.js
@@ -1,4 +1,4 @@
-// src/components/OnboardingWizard/OnboardingWizard.js
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getAdminConfig } from '../../api';
